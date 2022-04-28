817c20694937366c22061234108618b74db0c94d
Adrian Arcega <adrianarcega18@gmail.com>
2022-04-28 16:39:43 +0800

    first commit

---
+++ ARCEGA_OOP_NAME/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Adrian A. Arcega", "Hello! My name is", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                MessageBox.Show("Nice to meet you!");
+            }
+            else 
+            {
+                MessageBox.Show("Thank you!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Are you doing just fine?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                MessageBox.Show("That's good to know <3");
+            }
+            else 
+            {
+                MessageBox.Show("It's okay not to be okay, you'll be okay soon!");
+            }
+        }
+    }
+}
